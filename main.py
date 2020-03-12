cd59d9a58a48dc68e377f7cfa9635675a8a2aee9
Shane McDonough <shanemcdman@gmail.com>
2020-03-11 17:21:48 -0700

    Put main into fn

--- main.py
+++ main.py
@@ -3,31 +3,39 @@ import os
 from ball import Ball
 os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
 
-pygame.display.init()
-size = (900,600)
-screen = pygame.display.set_mode(size)
-colorscheme = 1
-balls = []
-for i in range(250):
-    balls.append(Ball(screen, (0,0),(0,0)))
-    balls[i].random(size)
-pygame.key.set_repeat(80)
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.unicode == "b":
-                for ball in balls:
-                    ball.burst(size)
-            elif event.unicode == "r":
-                for ball in balls:
-                    ball.random(size)
-            elif event.unicode >= "1" and event.unicode <= "9":
-                colorscheme = int(event.unicode)
-    screen.fill((0,0,0))
-    for ball in balls:
-        ball.update(size)
-        ball.draw(colorscheme)
-    pygame.display.update()
+def printcontrols():
+    pass
+
+def main():
+    printcontrols()
+    pygame.display.init()
+    size = (900,600)
+    screen = pygame.display.set_mode(size)
+    colorscheme = 1
+    balls = []
+    for i in range(250):
+        balls.append(Ball(screen, (0,0),(0,0)))
+        balls[i].random(size)
+    pygame.key.set_repeat(80)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.unicode == "b":
+                    for ball in balls:
+                        ball.burst(size)
+                elif event.unicode == "r":
+                    for ball in balls:
+                        ball.random(size)
+                elif event.unicode >= "1" and event.unicode <= "9":
+                    colorscheme = int(event.unicode)
+        screen.fill((0,0,0))
+        for ball in balls:
+            ball.update(size)
+            ball.draw(colorscheme)
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
