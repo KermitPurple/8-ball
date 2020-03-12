487db936cf3ea56c323a94a21e7e5e15ef3f7342
Shane McDonough <shanemcdman@gmail.com>
2020-03-11 17:07:20 -0700

    Add colorscheme 2

--- ball.py
+++ ball.py
@@ -46,6 +46,8 @@ class Ball:
         color = pygame.Color(255,255,255)
         if colorscheme == 1:
             color.hsva = ((self.pos[0]+self.pos[1])%360, 100, 100)
+        elif colorscheme == 2:
+            color.hsva = ((self.pos[0]-self.pos[1])%360, 100, 100)
         else: # default
             color.hsva = ((self.pos[0]+self.pos[1])%360, 100, 100)
         pygame.draw.circle(self.screen, color, p, self.r)
